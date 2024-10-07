3fa89cff2a3758d3732fd302b1abc8c38a84d148
NIPUN0BUGS <nipunsanjulasamarakoon@gmail.com>
2024-10-07 15:48:20 +0530

    Initial commit

---
+++ admin-portal-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import DriverForm from './components/DriverForm';
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <DriverForm />
+    </>
+  );
+};
+
+export default App;
